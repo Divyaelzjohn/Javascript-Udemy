5d379925367d011836de9576abd90d1bf88703ad
Divya <divelzjon@gmail.com>
2025-09-03 23:38:38 +0530

    JS2

---
+++ Section-2.1/script.js
@@ -0,0 +1,60 @@
+/* 
+1.Watch before you start!
+2.A brief introduction to javascript 
+3.Data Types
+4.Boolean Logic
+5.Javascript Releases:ES5,ES6+ and ESNNext
+6.Functions Calling Other Functions
+7.Reviewing Functions
+8.Learning How to Code
+9.How to think Like Developer
+10.Debugging(Fixing Errors)
+11.The Rise of AI Tools(ChatGPT,Copilot,Curesor AI)
+12.What's the DOM and DOM Manipulation
+13.An  high-level Overview of Javascript
+14.The JavaScript Engine and Runtime
+15.Execution Contexts and The Call Stack
+16.Scope and The Scope Chain
+17.Variable environment: Hoisting and The TDZ
+18.The this Keyword
+19.memory Management:Primitives vs Objects
+20.Memory Management:Garbage Collection
+21.Summary Which Data struction to Use?
+22.First-class and Higher-order Functions
+23.Closures
+24.Data Transformations:map,filter,reduce
+25.Summary:WHich array methods to use?
+26.How the DOM Really Works
+27.Event Propagation :Bubbling and Capturing
+28.Effecient Script Loadin defer and async
+29.What is object-oriented Programming?
+30.OOP in javascript
+31.Prototypal Inheritance and The Prototype Chain
+32.Object create
+33.Inheritance Between "Classes".Constructor Functions
+34.Inheritance Between "Classes".Objects.create
+35.ES6 Classes summary
+36.Mapty Projext:How to plan a web Project
+37.Mapty Project Final Consideration 
+38.Asynchronus JavaScript AJAX and APIs
+39.How the Web Works:Requests and Responses
+40.Promises and the Fetch API
+41.Asynchronus behind the scenes . The Event Loop
+42.An Overview of modern javascript Development
+43.An Overview of Modules in javascript
+44.Modern, clean and declarative javascript programming
+45.Forkify: Project Overview and planning
+46.The MVC Architecture
+47.Event handlers in MVC: Publicsher-subscriber Pattern 
+48.Forkify Project: Final Considerations
+*/
+
+/* Primitive Data Types
+1. Number: Floating point numbers - Used for decimals and integers (let age=23)
+2. String: Sequence of characters - Used for text (let firstName='Jonas')
+3. Boolean: Logical type that can only be true or false - Used for taking decisions(let fullAge=true);
+4.Undefined: Value taken by a variable that is not yet defined('empty value') (let children)
+5.Null:Also means 'empty value'
+6.Symbol(ES2015):Value that is unique and canot be changed [Not useful for now]
+7.BigInt(ES2020):Larger interger than the Number type can hold
+ */
