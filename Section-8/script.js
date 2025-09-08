07b7be9c2a5900080a1950aa5949eda7dc680be9
Divya <divelzjon@gmail.com>
2025-09-08 17:53:50 +0530

    JS-5->section9

---
+++ Section-8/script.js
@@ -0,0 +1,38 @@
+// Javascript is a high -level object-oriented , multi-paradigm programming language
+/*
+'use strict';
+function calcAge(birthYear){
+  const age=2037-birthYear;
+  // console.log(firstName);
+  function printAge(){
+    let output=`${firstName},You are ${age},born in ${birthYear}`;
+    console.log(output);  
+
+    if(birthYear>=1981 && birthYear<=1996){
+      var millenial=true;
+       //Creating NEW variable with same name as outer scope's variable
+      const firstName='Steven'
+      // Reassigning outer scop's variable
+      const output='NEW OUTPUT!';
+
+      const str=`Oh, and you're millenial,${firstName}`;
+      console.log(str);
+      
+      function add(a,b){
+        return a+b
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2,3))
+    console.log(output);
+  }
+  printAge();
+  return age;
+ }
+const firstName='Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge(); 
+*/
+
