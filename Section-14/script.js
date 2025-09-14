0cb8a8f2f6dd83a99af79ebfecdb72b37ec2f1c2
Divya <divelzjon@gmail.com>
2025-09-14 22:40:03 +0530

    JS-10.2

---
+++ Section-14/script.js
@@ -0,0 +1,182 @@
+'use strict'
+
+/*
+const Person=function(firstName, birthYear){
+  // console.log(this);
+
+  // Instance properties
+  this.firstName=firstName;
+  this.birthYear=birthYear
+
+  // // never to this
+  // this.calcAge=function(){
+  //   console.log(2037-this.birthYear)
+  // }
+}
+const jonas=new Person('jonas',1991);
+console.log(jonas)
+
+// 1.new {} is created
+// 2.function is called, this = {}
+// 3.{} linked to prototype
+// 4.function automatically return {}
+
+const matilda=new Person('Matilda',2017)
+const jack=new Person('Jack',1975)
+console.log(matilda,jack)
+
+// const jay='Jay'
+
+console.log(jonas instanceof Person)
+// console.log(jay instanceof Person)
+//////////////////////////////////////////
+// Prototypes
+console.log(Person.prototype)
+Person.prototype.calcAge=function(){
+  console.log(2037-this.birthYear)
+};
+jonas.calcAge();
+matilda.calcAge();
+console.log(jonas.__proto__)
+console.log(jonas.__proto__===Person.prototype)
+
+console.log(Person.prototype.isPrototypeOf(jonas))
+console.log(Person.prototype.isPrototypeOf(matilda))
+console.log(Person.prototype.isPrototypeOf(Person))
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species='Homo Sapiens';
+console.log(jonas.species,matilda.species)
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+/////////////////////////////////////////////
+//Prototypepal inheritance on Build-In Objects
+console.log(jonas.__proto__);
+// Object,prototype(top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__)
+
+console.dir(Person.prototype.constructor)
+
+const arr=[3,6,4,5,6,9,9]; // new Array===[]
+console.log(arr.__proto__)
+console.log(arr.__proto__===Array.prototype)
+
+console.log(arr.__proto__.__proto__)
+
+Array.prototype.unique=function(){
+  return[...new Set(this)]
+}
+console.log(arr.unique());
+
+const h1=document.querySelector('h1');
+console.dir(x=>x+1);
+*/
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+
+
+const Car=function(make, speed){
+  this.make=make;
+  this.speed=speed;
+};
+
+Car.prototype.accelerate=function(){
+  this.speed+=10;
+  console.log(`${this.make} is going at ${this.speed} km/h`)
+}
+
+Car.prototype.break=function(){
+  this.speed-=5;
+  console.log(`${this.make} is going at ${this.speed} km/h`)
+}
+
+const bmw=new Car('BMW',120)
+const mercedes=new Car('Mercedes',95)
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.break();
+
+*/
+
+// ES6 Classess
+
+// class expression
+// const PersonCl=class{}
+
+// class declaration
+class PersonCl{
+  constructor(fullName,birthYear){
+    this.fullName=fullName;
+    this.birthYear=birthYear;
+  }
+  // Methods will be added to .prototype property
+  calcAge(){
+    console.log(2037-this.birthYear)
+  }
+  greet(){
+    console.log(`Hey ${this.fullName}`);
+  }
+  get age(){
+    return 2037-this.birthYear;
+  }
+  // Set a property that already exists
+  set fullName(name){
+    console.log(name);
+    if(name.includes(' ')) this._fullName=name;
+    else alert(`${name} is not a full name!`)
+  }
+
+  get fullName(){
+    return this._fullName;
+  }
+}
+
+const jessica=new PersonCl('Jessica Davis',1996)
+console.log(jessica)
+jessica.calcAge()
+console.log(jessica.age)
+
+console.log(jessica.__proto__===PersonCl.prototype)
+
+// PersonCl.prototype.greet=function(){
+//   console.log(`Hey ${this.firstName}`);
+// }
+jessica.greet();
+
+// 1.classes are not hoisted
+// 2.classes are first-class citizens
+// 3.classes are executed in strict mode
+
+const walter = new PersonCl('Walter',1965)
+
+////////////////////////////////////////
+//setter and getters
+const account={
+  owner:'Jonas',
+  movements:[200,530,120,300],
+
+  get latest(){
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov){
+    return this.movements.push(mov);
+   }
+}
+console.log(account.latest)
+account.latest=50;
+console.log(account.movements);
