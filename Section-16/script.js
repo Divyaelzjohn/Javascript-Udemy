104df827dcb8aae219488579b61dd23f7191bd03
Divya <divelzjon@gmail.com>
2025-09-19 20:52:21 +0530

    JS-14.1

--- Section-16/script.js
+++ Section-16/script.js
@@ -364,4 +364,213 @@ Promise.resolve('Resolved promise 2').then(res=>{
 })
 console.log('Test end');
 */
+/*
+const lotteryPromise = new Promise(function(resolve, reject){
+  
+  console.log('Lotter draw is happening')
+
+  setTimeout(function(){
+    if(Math.random()>=0.5){
+      resolve('You WIN')
+    }else{
+      reject(new Error('You lost your money'))
+    }
+  },2000)
+});
+
+lotteryPromise.then(res=>console.log(res)).catch(err=>console.error(err))
+
+// Promisifying setTimeout
+const wait = function(seconds){
+  return new Promise(function(resolve){
+    setTimeout(resolve, seconds*1000)
+  })
+}
+
+wait(1)
+.then(()=>{
+  console.log('1 second passed');
+  return wait(1);
+})
+.then(()=>{
+  console.log('2 second passed');
+  return wait(1);
+})
+.then(()=>{
+  console.log('3 second passed');
+  return wait(1);
+})
+.then(()=>console.log('4 second passed'))
+
+Promise.resolve('abc').then(x=>console.log(x))
+Promise.reject(new Error('Problem!')).catch(x=>console.error(x))
+*/
+
+/*
+const getPosition=function(){
+  return new Promise(function(resolve, reject){
+    // navigator.geolocation.getCurrentPosition(
+    //   position=> resolve(position),
+    //   err=>reject(err))
+    // // console.log('Getting position')
+
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
+}
+
+// getPosition().then(pos=>console.log(pos));
+
+
+const whereAmI = function(){
+
+  getPosition().then(pos=>{
+    // console.log(pos.coords)
+    const {latitude:lat, longitude:lng} = pos.coords;
+    return  fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+  })
+ 
+  .then(res=>{
+    if(!res.ok) throw new Error(`Problem with geocoding ${res.status}`)
+    return res.json()
+  })
+  .then(data=>{
+    console.log(data);
+    console.log(`You are in ${data.city}, ${data.countryName}`)
+
+    return fetch(`https://restcountries.com/v2/name/${data.countryName}`);
+  })
+  .then(res=> {
+      if(!response.ok)
+        throw new Error(`Country not found (${response.status})`)
+  
+      return res.json()})
+    .then(data => renderCountry(data[0]))
+  .catch(err=>console.log(`${err.message} 💥`));
+}
+
+btn.addEventListener('click',whereAmI())
+
+*/
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that );
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+*/
+/*
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
+*/
+////////////////////////////////////////////////////////////////////
+// Consuming Promises with async/Await
+// Error Handling With try...catch
+/*
+const getPosition=function(){
+  return new Promise(function(resolve, reject){
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
+}
+
+const whereAmI=async function(country){
+  // fetch(`https://restcountries.com/v2/name/${country}`).then(res=>console.log(res))
+  const {} = await getPosition();
+
+  const res =  await fetch(`https://restcountries.com/v2/name/${country}`) ;
+  // console.log(res);
+  const data = await res.json()
+  console.log(data);
+  renderCountry(data[0])
+
+}
+whereAmI('portugal')
+console.log('FIRST')*/
+
+//////////////////////////////////////////////////////////////////
+// Returning Valus from Async Function
+
+const getPosition = function(){
+  return new Promise(function(resolve, reject){
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+    
+  })
+}
+
+// fetch(`https://restcountries.com/v2/name/${country}).then(res=>console.log(res))
+
+const whereAmI=async function(){
+  try{
+    //geolocation
+    const pos=await getPosition();
+    const {latitude:lat, longitude:lng}= pos.coords;
+
+    // Reverse geocoding
+    const resGeo=await fetch(`https://api.bigdataloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+    if(!resGeo.ok) throw new Error('Problem getting location data');
+
+    const dataGeo=await resGeo.json()
+    console.log(dataGeo)
+  }
+}
 
